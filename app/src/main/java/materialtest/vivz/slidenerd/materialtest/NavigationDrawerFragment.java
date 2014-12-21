6de5b1e2721ff867e7318354cee2a2324291f1be
slidenerd <slidenerd@gmail.com>
2014-12-21 21:43:10 +0530

    With Translucent Status Bar Hack for kitkat and lollipop

--- app/src/main/java/materialtest/vivz/slidenerd/materialtest/NavigationDrawerFragment.java
+++ app/src/main/java/materialtest/vivz/slidenerd/materialtest/NavigationDrawerFragment.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -21,6 +22,7 @@ import android.view.animation.AnimationUtils;
  */
 public class NavigationDrawerFragment extends Fragment {
 
+    private RecyclerView recyclerView;
     public static final String PREF_FILE_NAME="testpref";
     public static final String KEY_USER_LEARNED_DRAWER="user_learned_drawer";
     private ActionBarDrawerToggle mDrawerToggle;
@@ -47,7 +49,9 @@ public class NavigationDrawerFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_navigation_drawer, container, false);
+        View layout=inflater.inflate(R.layout.fragment_navigation_drawer, container, false);
+        recyclerView= (RecyclerView) layout.findViewById(R.id.drawerList);
+        return layout;
     }
 
     public void setUp(int fragmentId, DrawerLayout drawerLayout, final Toolbar toolbar) {
@@ -74,6 +78,7 @@ public class NavigationDrawerFragment extends Fragment {
 
             @Override
             public void onDrawerSlide(View drawerView, float slideOffset) {
+                super.onDrawerSlide(drawerView, slideOffset);
 //                if(slideOffset<0.6) {
 //                    toolbar.setAlpha(1 - slideOffset);
 //                }
