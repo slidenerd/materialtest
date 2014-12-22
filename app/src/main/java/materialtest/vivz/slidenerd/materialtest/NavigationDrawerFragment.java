54413fd16283b34c4c02205c83e568776e0b6f57
slidenerd <slidenerd@gmail.com>
2014-12-22 18:29:13 +0530

    Fully Working RecyclerView inside the NavigationDrawer

--- app/src/main/java/materialtest/vivz/slidenerd/materialtest/NavigationDrawerFragment.java
+++ app/src/main/java/materialtest/vivz/slidenerd/materialtest/NavigationDrawerFragment.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -15,6 +17,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 
 /**
@@ -27,7 +33,7 @@ public class NavigationDrawerFragment extends Fragment {
     public static final String KEY_USER_LEARNED_DRAWER="user_learned_drawer";
     private ActionBarDrawerToggle mDrawerToggle;
     private DrawerLayout mDrawerLayout;
-
+    private VivzAdapter adapter;
     private boolean mUserLearnedDrawer;
     private boolean mFromSavedInstanceState;
     private View containerView;
@@ -51,9 +57,25 @@ public class NavigationDrawerFragment extends Fragment {
         // Inflate the layout for this fragment
         View layout=inflater.inflate(R.layout.fragment_navigation_drawer, container, false);
         recyclerView= (RecyclerView) layout.findViewById(R.id.drawerList);
+        adapter=new VivzAdapter(getActivity(),getData());
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
         return layout;
     }
 
+    public static List<Information> getData(){
+        List<Information> data=new ArrayList<>();
+        int[] icons={R.drawable.ic_number1,R.drawable.ic_number2,R.drawable.ic_number3,R.drawable.ic_number4};
+        String[] titles={"Vivz","Anky","Slidenerd","YouTube"};
+        for(int i=0;i<titles.length && i<icons.length;i++)
+        {
+            Information current=new Information();
+            current.iconId=icons[i];
+            current.title=titles[i];
+            data.add(current);
+        }
+        return data;
+    }
     public void setUp(int fragmentId, DrawerLayout drawerLayout, final Toolbar toolbar) {
         containerView=getActivity().findViewById(fragmentId);
         mDrawerLayout=drawerLayout;
