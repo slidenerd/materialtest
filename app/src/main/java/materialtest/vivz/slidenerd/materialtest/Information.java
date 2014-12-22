ff36ee2bf0d22beb17eb6d8673bc6fca4b3d83ee
slidenerd <slidenerd@gmail.com>
2014-12-22 18:37:53 +0530

    Fully Working RecyclerView inside the NavigationDrawer

---
+++ app/src/main/java/materialtest/vivz/slidenerd/materialtest/Information.java
@@ -0,0 +1,9 @@
+package materialtest.vivz.slidenerd.materialtest;
+
+/**
+ * Created by Windows on 22-12-2014.
+ */
+public class Information {
+    int iconId;
+    String title;
+}
---
+++ app/src/main/java/materialtest/vivz/slidenerd/materialtest/VivzAdapter.java
@@ -0,0 +1,51 @@
+package materialtest.vivz.slidenerd.materialtest;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Created by Windows on 22-12-2014.
+ */
+public class VivzAdapter extends RecyclerView.Adapter<VivzAdapter.MyViewHolder> {
+    List<Information> data= Collections.emptyList();
+    private LayoutInflater inflater;
+    public VivzAdapter(Context context, List<Information> data){
+        inflater=LayoutInflater.from(context);
+        this.data=data;
+    }
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view=inflater.inflate(R.layout.custom_row, parent,false);
+        MyViewHolder holder=new MyViewHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+        Information current=data.get(position);
+        holder.title.setText(current.title);
+        holder.icon.setImageResource(current.iconId);
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.size();
+    }
+    class MyViewHolder extends RecyclerView.ViewHolder{
+        TextView title;
+        ImageView icon;
+        public MyViewHolder(View itemView) {
+            super(itemView);
+            title= (TextView) itemView.findViewById(R.id.listText);
+            icon= (ImageView) itemView.findViewById(R.id.listIcon);
+        }
+    }
+}
