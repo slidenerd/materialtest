cb55bf1399bee52ddcd1f8ba70310aa47f3877b2
slidenerd <slidenerd@gmail.com>
2015-01-23 12:16:11 +0530

    made 2 separate ripple files, one for the recyclerview s custom item and another for the tab, changed the tab's ripple file to contain only the mask

--- app/src/main/java/materialtest/vivz/slidenerd/materialtest/MainActivity.java
+++ app/src/main/java/materialtest/vivz/slidenerd/materialtest/MainActivity.java
@@ -1,6 +1,7 @@
 package materialtest.vivz.slidenerd.materialtest;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -10,6 +11,9 @@ import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.style.ImageSpan;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -37,11 +41,16 @@ public class MainActivity extends ActionBarActivity {
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         NavigationDrawerFragment drawerFragment = (NavigationDrawerFragment)
                 getSupportFragmentManager().findFragmentById(R.id.fragment_navigation_drawer);
-        drawerFragment.setUp(R.id.fragment_navigation_drawer,(DrawerLayout)findViewById(R.id.drawer_layout), toolbar);
+        drawerFragment.setUp(R.id.fragment_navigation_drawer, (DrawerLayout) findViewById(R.id.drawer_layout), toolbar);
 
-        mPager= (ViewPager) findViewById(R.id.pager);
+        mPager = (ViewPager) findViewById(R.id.pager);
         mPager.setAdapter(new MyPagerAdapter(getSupportFragmentManager()));
-        mTabs= (SlidingTabLayout) findViewById(R.id.tabs);
+        mTabs = (SlidingTabLayout) findViewById(R.id.tabs);
+        mTabs.setCustomTabView(R.layout.custom_tab_view, R.id.tabText);
+        mTabs.setDistributeEvenly(true);
+        mTabs.setSelectedIndicatorColors(getResources().getColor(R.color.colorAccent));
+        mTabs.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+
         mTabs.setViewPager(mPager);
 
     }
@@ -73,46 +82,54 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public static class MyFragment extends Fragment{
+    public static class MyFragment extends Fragment {
         private TextView textView;
-        public static MyFragment getInstance(int position){
-            MyFragment myFragment=new MyFragment();
-            Bundle args=new Bundle();
-            args.putInt("position",position);
+
+        public static MyFragment getInstance(int position) {
+            MyFragment myFragment = new MyFragment();
+            Bundle args = new Bundle();
+            args.putInt("position", position);
             myFragment.setArguments(args);
             return myFragment;
         }
 
         @Override
         public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-            View layout=inflater.inflate(R.layout.fragment_my, container, false);
-            textView= (TextView) layout.findViewById(R.id.position);
-            Bundle bundle=getArguments();
-            if(bundle!=null)
-            {
-                textView.setText("The Page Selected Is "+bundle.getInt("position"));
+            View layout = inflater.inflate(R.layout.fragment_my, container, false);
+            textView = (TextView) layout.findViewById(R.id.position);
+            Bundle bundle = getArguments();
+            if (bundle != null) {
+                textView.setText("The Page Selected Is " + bundle.getInt("position"));
             }
             return layout;
         }
     }
 
-    class MyPagerAdapter extends FragmentPagerAdapter{
+    class MyPagerAdapter extends FragmentPagerAdapter {
 
-        String[] tabs;
+        int icons[] = {R.drawable.ic_action_home, R.drawable.ic_action_articles, R.drawable.ic_action_personal};
+        String[] tabText = getResources().getStringArray(R.array.tabs);
+
         public MyPagerAdapter(FragmentManager fm) {
             super(fm);
-            tabs=getResources().getStringArray(R.array.tabs);
+            tabText = getResources().getStringArray(R.array.tabs);
         }
 
         @Override
         public Fragment getItem(int position) {
-            MyFragment myFragment=MyFragment.getInstance(position);
+            MyFragment myFragment = MyFragment.getInstance(position);
             return myFragment;
         }
 
         @Override
         public CharSequence getPageTitle(int position) {
-            return tabs[position];
+
+            Drawable drawable=getResources().getDrawable(icons[position]);
+            drawable.setBounds(0,0,36,36);
+            ImageSpan imageSpan=new ImageSpan(drawable);
+            SpannableString spannableString=new SpannableString(" ");
+            spannableString.setSpan(imageSpan,0,spannableString.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+            return spannableString;
         }
 
         @Override
